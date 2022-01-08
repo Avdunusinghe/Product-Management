6243f0230fe96ea7e669e6292395721b16861bc1
Avdunusinghe <avdunusinghe@gmail.com>
2022-01-08 21:35:46 +0530

    model and web services Partially completed

---
+++ controllers/Product.controllers.js
@@ -0,0 +1,35 @@
+const Product = require("../models/Product.model");
+
+const saveProduct = (request, response) => {
+  const { name, companyName, discription } = request.body;
+
+  const product = new Product({
+    name,
+    companyName,
+    discription,
+  });
+
+  product
+    .save()
+    .then((product) => {
+      response.json(product);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
+};
+
+const getAllProductDetails = (request, response) => {
+  Product.find()
+    .then((products) => {
+      response.json(products);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
+};
+
+module.exports = {
+  saveProduct,
+  getAllProductDetails,
+};
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 mongoose
   .connect(
     "mongodb+srv://sa:sa123@cluster0.7q8er.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
