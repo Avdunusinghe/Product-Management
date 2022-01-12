66bd8b4e1ed2526a97696e2c7d2216fdb2c1b47c
Avdunusinghe <avdunusinghe@gmail.com>
2022-01-12 13:34:42 +0530

    Add Swagger Api Documentation

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ mongoose
 
 app.use("/api/product", require("./routes/Product.route"));
 const PORT = process.env.PORT || 5000;
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
   console.log("Server is Running");
 });
