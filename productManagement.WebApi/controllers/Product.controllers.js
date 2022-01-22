8cbf02a129f15234822cd5f193ee992da27e1087
Avdunusinghe <avdunusinghe@gmail.com>
2022-01-22 10:37:06 +0530

    state changes

--- productManagement.WebApi/controllers/Product.controllers.js
+++ productManagement.WebApi/controllers/Product.controllers.js
@@ -1,4 +1,4 @@
-const Product = require("../models/Product.model");
+const Product = require("../../productManagement.Models/models/Product.model");
 
 const saveProduct = (request, response) => {
   const { name, companyName, discription } = request.body;
